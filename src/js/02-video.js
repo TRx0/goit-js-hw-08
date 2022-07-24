cf76225f82f216931ecae1583bc74775a503384a
Ta <tarasragumov28@gmail.com>
2022-07-24 22:21:55 +0300

    виконав дз

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Vimeo from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+    const iframe = document.querySelector('iframe');
+    
+    const player = new Vimeo(iframe);
+       
+    const onPlay = function(data) {
+        const time = data.seconds;
+        localStorage.setItem("videoplayer-current-time", time);
+    };
+
+    const savedTime = localStorage.getItem("videoplayer-current-time");  
+
+    player.on('timeupdate', throttle(onPlay, 1000));
+    if(savedTime) {
+        player.setCurrentTime(savedTime);
+    }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onInputChange, 500));
+form.addEventListener('submit', onFormSubmit);
+
+saveForm();
+
+const formData = {}; 
+
+function onInputChange(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    formData.email = input.value;
+    formData.message = textarea.value;
+    console.log(formData);
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function saveForm() {
+    const rawValue = localStorage.getItem(STORAGE_KEY);
+    const valuesObject = JSON.parse(rawValue);
+   
+if(valuesObject) {
+    const savedEmail = valuesObject.email;
+    input.value = savedEmail;
+
+    const savedMessage = valuesObject.message;
+    message.value = savedMessage;
+};
+};
